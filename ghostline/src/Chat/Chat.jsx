b82b23fac4f003f2ccba471c81c7516cbf97f3d3
Mohd Awresh <107464990+Awresh@users.noreply.github.com>
2023-07-05 22:54:43 +0530

    logo

--- ghostline/src/Chat/Chat.jsx
+++ ghostline/src/Chat/Chat.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import MessageContainer from "../Message/MessageContainer";
 import ConversationPanel from "../Message/ConversationPanel";
-import G1 from '../assets/G1.png'
-import G2 from '../assets/G2.png'
-import G3 from '../assets/G5.png'
-import G4 from '../assets/G4.png'
+import G1 from "../assets/G1.png";
+import G2 from "../assets/G2.png";
+import G3 from "../assets/G5.png";
+import G4 from "../assets/G4.png";
 import "./Chat.css";
 
 const Chat = () => {
@@ -13,23 +13,24 @@ const Chat = () => {
     {
       avatar: G1,
       nickname: "G1",
-      message: ["Somewhere stored deep, deep in my memory banks is the phrase 'It really whips the llama's ass'."],
+      messages: [
+        "Somewhere stored deep, deep in my memory banks is the phrase 'It really whips the llama's ass'.",
+      ],
     },
     {
       avatar: G3,
       nickname: "G3",
-      message: ["Think the guy that did the voice has a Twitter?"],
+      messages: ["Think the guy that did the voice has a Twitter?"],
     },
     {
       avatar: G2,
-      nickname: "G2",
       messages: ["WE MUST FIND HIM!!", "Wait ..."],
     },
     {
       reversed: true,
       avatar: G4,
       nickname: "G4",
-      message: ["Winamp's still an essential."],
+      messages: ["Winamp's still an essential."],
     },
     {
       avatar: G2,
@@ -38,8 +39,7 @@ const Chat = () => {
     },
     {
       avatar: G3,
-      nickname: "G3",
-      message:[ "I am from Kanpur"],
+      messages: ["I am from Kanpur"],
     },
     {
       avatar: G2,
@@ -49,8 +49,81 @@ const Chat = () => {
     {
       reversed: true,
       avatar: G4,
-      nickname: "G4",
       messages: ["Ok Ok i got it", "I am from Gorakhpur"],
+    },
+    // Additional messages
+    {
+      avatar: G1,
+      nickname: "G1",
+      messages: ["Do you have any favorite music genres?"],
+    },
+    {
+      avatar: G2,
+      nickname: "G2",
+      messages: [
+        "I enjoy listening to various genres like pop, rock, and electronic music.",
+      ],
+    },
+    {
+      avatar: G3,
+      nickname: "G3",
+      messages: ["I'm a fan of hip-hop and R&B."],
+    },
+    {
+      reversed: true,
+      avatar: G4,
+      nickname: "G4",
+      messages: ["I love classical music and jazz."],
+    },
+    // More additional messages
+    {
+      avatar: G1,
+      nickname: "G1",
+      messages: ["That's cool! Music is so diverse and wonderful."],
+    },
+    {
+      avatar: G2,
+      nickname: "G2",
+      messages: ["Absolutely! It's a universal language that connects people."],
+    },
+    {
+      avatar: G3,
+      nickname: "G3",
+      messages: [
+        "I completely agree. Music has the power to evoke emotions and memories.",
+      ],
+    },
+    {
+      reversed: true,
+      avatar: G4,
+      nickname: "G4",
+      messages: ["Indeed. Each genre has its own unique charm."],
+    },
+    {
+      avatar: G1,
+      nickname: "G1",
+      messages: ["By the way, do any of you play any musical instruments?"],
+    },
+    {
+      avatar: G2,
+      nickname: "G2",
+      messages: ["I play the guitar. What about you guys?"],
+    },
+    {
+      
+      avatar: G3,
+      nickname: "G3",
+      messages: [
+        "I'm learning to play the piano. It's challenging, but I love it.",
+      ],
+    },
+    {
+      reversed: true,
+      avatar: G4,
+      nickname: "G4",
+      messages: [
+        "I used to play the violin when I was younger. It's been a while though.",
+      ],
     },
   ];
 
