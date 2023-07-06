8489093fbf38dbeee1b7489505d3169747c4b8e9
Mohd Awresh <107464990+Awresh@users.noreply.github.com>
2023-07-06 15:03:35 +0530

    timeStamp

--- ghostline/src/Chat/Chat.jsx
+++ ghostline/src/Chat/Chat.jsx
@@ -16,48 +16,57 @@ const Chat = () => {
       messages: [
         "Somewhere stored deep, deep in my memory banks is the phrase 'It really whips the llama's ass'.",
       ],
+      time:"12:02"
     },
     {
       avatar: G3,
       nickname: "G3",
       messages: ["Think the guy that did the voice has a Twitter?"],
+      time:"12:02"
     },
     {
       avatar: G2,
       nickname: "G2",
       messages: ["WE MUST FIND HIM!!", "Wait ..."],
+      time:"12:02"
     },
     {
       reversed: true,
       avatar: G4,
       nickname: "G4",
       messages: ["Winamp's still an essential."],
+      time:"12:02"
     },
     {
       avatar: G2,
       nickname: "G2",
       messages: ["Are U From", "Wait ..."],
+      time:"12:02"
     },
     {
       avatar: G3,
       nickname: "G3",
       messages: ["I am from Kanpur"],
+      time:"12:02"
     },
     {
       avatar: G2,
       nickname: "G2",
       messages: ["And I am from Lucknow"],
+      time:"12:02"
     },
     {
       reversed: true,
       avatar: G4,
       messages: ["Ok Ok i got it", "I am from Gorakhpur"],
+      time:"12:02"
     },
     // Additional messages
     {
       avatar: G1,
       nickname: "G1",
       messages: ["Do you have any favorite music genres?"],
+      time:"12:02"
     },
     {
       avatar: G2,
@@ -65,28 +74,33 @@ const Chat = () => {
       messages: [
         "I enjoy listening to various genres like pop, rock, and electronic music.",
       ],
+      time:"12:02"
     },
     {
       avatar: G3,
       nickname: "G3",
       messages: ["I'm a fan of hip-hop and R&B."],
+      time:"12:02"
     },
     {
       reversed: true,
       avatar: G4,
       nickname: "G4",
       messages: ["I love classical music and jazz."],
+      time:"12:02"
     },
     // More additional messages
     {
       avatar: G1,
       nickname: "G1",
       messages: ["That's cool! Music is so diverse and wonderful."],
+      time:"12:02"
     },
     {
       avatar: G2,
       nickname: "G2",
       messages: ["Absolutely! It's a universal language that connects people."],
+      time:"12:02"
     },
     {
       avatar: G3,
@@ -94,22 +108,26 @@ const Chat = () => {
       messages: [
         "I completely agree. Music has the power to evoke emotions and memories.",
       ],
+      time:"12:02"
     },
     {
       reversed: true,
       avatar: G4,
       nickname: "G4",
       messages: ["Indeed. Each genre has its own unique charm."],
+      time:"12:02"
     },
     {
       avatar: G1,
       nickname: "G1",
       messages: ["By the way, do any of you play any musical instruments?"],
+      time:"12:02"
     },
     {
       avatar: G2,
       nickname: "G2",
       messages: ["I play the guitar. What about you guys?"],
+      time:"12:02"
     },
     {
       
@@ -118,6 +136,7 @@ const Chat = () => {
       messages: [
         "I'm learning to play the piano. It's challenging, but I love it.",
       ],
+      time:"12:02"
     },
     {
       reversed: true,
@@ -126,6 +145,98 @@ const Chat = () => {
       messages: [
         "I used to play the violin when I was younger. It's been a while though.",
       ],
+      time:"12:02"
+    },
+    {
+      reversed: true,
+      avatar: G4,
+      nickname: "G4",
+      messages: [
+        "I used to play the violin when I was younger. It's been a while though.",
+      ],
+      time:"12:02"
+    },
+    {
+      reversed: true,
+      avatar: G4,
+      nickname: "G4",
+      messages: [
+        "I used to play the violin when I was younger. It's been a while though.",
+      ],
+      time:"12:02"
+    },
+    {
+      reversed: true,
+      avatar: G4,
+      nickname: "G4",
+      messages: [
+        "I used to play the violin when I was younger. It's been a while though.",
+      ],
+      time:"12:02"
+    },
+    {
+      avatar: G2,
+      nickname: "G2",
+      messages: ["I play the guitar. What about you guys?"],
+      time:"12:02"
+    }, {
+      avatar: G2,
+      nickname: "G2",
+      messages: ["I play the guitar. What about you guys?"],
+      time:"12:02"
+    },
+    {
+      
+      avatar: G3,
+      nickname: "G3",
+      messages: [
+        "I'm learning to play the piano. It's challenging, but I love it.",
+      ],
+      time:"12:02"
+    },
+    {
+      reversed: true,
+      avatar: G4,
+      nickname: "G4",
+      messages: [
+        "I used to play the violin when I was younger. It's been a while though.",
+      ],
+      time:"12:02"
+    },{
+      
+      avatar: G3,
+      nickname: "G3",
+      messages: [
+        "I'm learning to play the piano. It's challenging, but I love it.",
+      ],
+      time:"12:02"
+    },
+    {
+      reversed: true,
+      avatar: G4,
+      nickname: "G4",
+      messages: [
+        "I used to play the violin when I was younger. It's been a while though.",
+      ],
+      time:"12:02"
+    },
+    {
+      
+      avatar: G3,
+      nickname: "G3",
+      messages: [
+        "I'm learning to play the piano. It's challenging, but I love it.",
+      ],
+      time:"12:02"
+    },
+    {
+      reversed: true,
+      avatar: G4,
+      nickname: "G4",
+      messages: [
+        "I used to play the violin when I was younger. It's been a while though.",
+      ],
+      time:"12:02"
     },
   ];
 
@@ -141,6 +252,7 @@ const Chat = () => {
             message={message.message}
             messages={message.messages}
             reversed={message.reversed}
+            time={message.time}
           />
         ))}
       </div>
--- ghostline/src/Message/MessageContainer.jsx
+++ ghostline/src/Message/MessageContainer.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import ReactionButton from "./ReactionButton";
 import ReplyButton from "./ReplyButton";
 
-const MessageContainer = ({ avatar, nickname, message, messages, reversed }) => {
+const MessageContainer = ({
+  avatar,
+  nickname,
+  message,
+  messages,
+  reversed,
+  time,
+}) => {
   return (
-    <div className={`chat__conversation-board__message-container ${reversed ? "reversed" : ""}`}>
+    <div
+      className={`chat__conversation-board__message-container ${
+        reversed ? "reversed" : ""
+      }`}
+    >
       <div className="chat__conversation-board__message__person">
         <div className="chat__conversation-board__message__person__avatar">
           <img src={avatar} alt={nickname} />
@@ -17,6 +28,7 @@ const MessageContainer = ({ avatar, nickname, message, messages, reversed }) => 
         {message && (
           <div className="chat__conversation-board__message__bubble">
             <span>{message}</span>
+            <p>{time}</p>
             <div className="chat__conversation-board__message__options">
               <ReactionButton />
               <ReplyButton />
@@ -25,8 +37,17 @@ const MessageContainer = ({ avatar, nickname, message, messages, reversed }) => 
         )}
         {messages &&
           messages.map((msg, index) => (
-            <div className="chat__conversation-board__message__bubble" key={index}>
-              <span>{msg}</span>
+            <div
+              className="chat__conversation-board__message__bubble"
+              key={index}
+            >
+              <p>
+                <span style={{fontSize:'7px'}}>
+                  {time}
+                </span>
+                {msg}
+              </p>
+
               <div className="chat__conversation-board__message__options">
                 <ReactionButton />
                 <ReplyButton />
