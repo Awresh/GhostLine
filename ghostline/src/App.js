2824bcfc41fec82f9471e776591fe83da4f8887e
Awresh <experiments.worlds@gmail.com>
2023-11-23 11:29:23 +0530

    add

--- ghostline/src/App.js
+++ ghostline/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chat from "./Chat/Chat";
 import Home from "./Chat/Home";
+import Chat1 from "./Chat/Chat1";
 
 export default function App() {
   return (
     <BrowserRouter>
-     <Home/>
+     {/* <Home/> */}
+     <Chat1/>
     </BrowserRouter>
   );
 }
