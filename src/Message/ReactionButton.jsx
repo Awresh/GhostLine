57f874f188db89c0ccba1e1b12f8c22a84a066dd
Awresh <experiments.worlds@gmail.com>
2024-01-04 13:24:53 +0530

    update

---
+++ src/Message/ReactionButton.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ReactionButton = () => {
+  return (
+    <button className="btn-icon chat__conversation-board__message__option-button option-item emoji-button reaction">
+      {/* Emoji button icon */}
+      <svg
+        className="feather feather-smile sc-dnqmqq jxshSx"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        aria-hidden="true"
+      >
+        <circle cx="12" cy="12" r="10"></circle>
+        <path d="M8 14s1.5 2 4 2 4-2 4-2"></path>
+        <line x1="9" y1="9" x2="9.01" y2="9"></line>
+        <line x1="15" y1="9" x2="15.01" y2="9"></line>
+      </svg>
+    </button>
+  );
+};
+
+export default ReactionButton;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// Function to update the theme based on the device's color scheme preference
+const updateTheme = (event) => {
+  if (event.matches) {
+    document.body.classList.remove('--light-theme');
+    document.body.classList.add('--dark-theme');
+  } else {
+    document.body.classList.remove('--dark-theme');
+    document.body.classList.add('--light-theme');
+  }
+};
+
+// Create a media query to track changes in the prefers-color-scheme
+const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+// Initial theme update
+updateTheme(colorSchemeQuery);
+
+// Listen for changes in the prefers-color-scheme and update the theme
+colorSchemeQuery.addEventListener('change', updateTheme)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
